eca0b9afbf0f1af004d8deee89f8d688cd9679fd
ViniciusDuranteBagio <viniciusduranteb@gmail.com>
2024-09-25 22:42:00 -0300

    Creating Example controllers, endpoint and model

---
+++ src/main/java/com/todo_list/todo_list/model/Example.java
@@ -0,0 +1,13 @@
+package com.todo_list.todo_list.model;
+
+public class Example {
+
+    private String name;
+
+    public Example() {
+    }
+
+    public String getName() {
+        return name;
+    }
+}
